982173b4e537130326dbc419d1e4518b9c9984b6
paweln1975 <pawel.niedziela@gmail.com>
2022-02-23 23:48:32 +0100

    basic tests of creating users

--- app/core/models.py
+++ app/core/models.py
@@ -7,8 +7,19 @@ from django.contrib.auth.models import PermissionsMixin
 class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("User must have an e-mail address.")
+
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password=None):
+        user = self.create_user(email=email, password=password)
+        user.is_superuser = True
+        user.is_staff = True
         user.save(using=self._db)
 
         return user
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -22,3 +22,21 @@ class ModelTests(TestCase):
             password=p
         )
         self.assertEqual(user.email, email.lower())
+
+    def test_invalid_email_raises_error(self):
+        with self.assertRaises(ValueError):
+            p = "88383883s"
+            get_user_model().objects.create_user(
+                email=None,
+                password=p
+            )
+
+    def test_create_superuser(self):
+        email = "test@TEST.COM"
+        p = "88383883s"
+        user = get_user_model().objects.create_superuser(
+            email=email,
+            password=p
+        )
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
