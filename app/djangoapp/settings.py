c9000e59f92b3511883492b0fef61e477e99ef3b
paweln1975 <pawel.niedziela@gmail.com>
2022-03-07 20:30:33 +0100

    create user api tests

--- app/djangoapp/settings.py
+++ app/djangoapp/settings.py
@@ -32,6 +32,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'rest_framework',
+    'rest_framework.authtoken',
+    'user',
     'core',
     'django.contrib.admin',
     'django.contrib.auth',
